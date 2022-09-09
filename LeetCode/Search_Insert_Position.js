225f973e2c780e136794bd51df8cabfa63b34d6f
martincorona007 <martincorona007@hotmail.com>
2022-09-08 19:51:30 -0500

    LC 35 Serch Insert Position - [UPDATE solution]

--- LeetCode/Search_Insert_Position.js
+++ LeetCode/Search_Insert_Position.js
@@ -14,3 +14,22 @@
   }
   return nums.length;
 };
+
+//Runtime: 99 ms, faster than 31.09% of JavaScript online submissions for Search Insert Position.
+//Memory Usage: 42.3 MB, less than 48.98% of JavaScript online submissions for Search Insert Position.
+var searchInsert = function(nums, target) {
+  let right = nums.length - 1;
+  let left = 0;
+  let middle = 0; 
+  while(left <= right){
+      middle = Math.floor((left + right)/2);
+      if(nums[middle]===target) return middle;
+      if(nums[middle] < target) left = middle + 1;
+      else right = middle - 1
+  }
+  console.log("out 1 ",middle);
+  console.log("out 2 ",left);
+  console.log("out 3 ",right);
+  return left;
+  
+};
