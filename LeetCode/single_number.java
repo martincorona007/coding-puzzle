3137ba841e643b8f163b52bd2dd7aeee0be0efa3
martincorona007 <martincorona007@hotmail.com>
2022-09-24 15:58:48 -0500

    LC 136 Single Number

---
+++ LeetCode/single_number.java
@@ -0,0 +1,40 @@
+//Runtime: 21 ms, faster than 22.14% of Java online submissions for Single Number.
+//Memory Usage: 53.4 MB, less than 24.58% of Java online submissions for Single Number.
+class Solution {
+    HashSet<Integer> singleNumber = new HashSet<Integer>();
+    public int singleNumber(int[] nums) {
+        
+        for(int i = 0; i < nums.length;i++){
+           if(singleNumber.contains(nums[i])){
+                singleNumber.remove(nums[i]);
+            }else{
+                singleNumber.add(nums[i]);
+            }
+        }
+        for(Integer iter: singleNumber){
+            return iter;
+        }
+        return -1;
+    }
+}
+//Runtime: 41 ms, faster than 8.87% of Java online submissions for Single Number.
+//Memory Usage: 53.7 MB, less than 23.31% of Java online submissions for Single Number.
+class Solution {
+    HashSet<Integer> singleNumber = new HashSet<Integer>();
+    public int singleNumber(int[] nums) {
+        
+        for(int i = 0; i < nums.length;i++){
+            if(singleNumber.size() == 0){
+                singleNumber.add(nums[i]);
+            }else if(singleNumber.contains(nums[i])){
+                singleNumber.remove(nums[i]);
+            }else{
+                singleNumber.add(nums[i]);
+            }
+        }
+        for(Integer iter: singleNumber){
+            return iter;
+        }
+        return 0;
+    }
+}
