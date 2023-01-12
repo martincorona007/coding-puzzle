7d8fd9a93d7b4f2a5ad6008a83957b249bc322c6
martincorona007 <martincorona007@hotmail.com>
2023-01-12 16:44:46 -0600

    LC 2529 Maximum Count of Positive Integer and Negative Integer

---
+++ LeetCode/maxium_count_of_positive_integer_and_negative_integer.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/submissions/877075498/
+class Solution {
+    public int maximumCount(int[] nums) {
+        int positive = 0;
+        int negative = 0;
+        for(int i = 0; i < nums.length; i++){
+                if( nums[i] < 0){
+                     negative++;
+                }
+                if(nums[i] > 0){
+                    positive++;
+                } 
+             
+        }
+        return (positive >= negative)? positive : negative;
+    }
+}
