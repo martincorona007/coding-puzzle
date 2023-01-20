acc08603a4e3dd9b2f704c41ff1fab3207231128
martincorona007 <martincorona007@hotmail.com>
2023-01-20 13:14:20 -0600

    HR 2DArrays - DS and Array - DS

---
+++ HackerRank/2DArrayDS.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function hourglassSum(arr) {
+    // Write your code here
+    console.log(arr)
+    console.log(arr[0])
+    console.log(arr[1])
+    console.log(arr[3])
+    console.log(arr[3,4])
+    console.log(arr[3][0])
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = hourglassSum(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
