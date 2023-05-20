f8c4ba64254a8ebbc48976bebb3bb57d09cfb798
martincorona007 <martincorona007@hotmail.com>
2023-05-20 11:47:25 -0600

    fix typo

--- HackerRank/Java_String_Reverse.java
+++ HackerRank/Java_String_Reverse.java
@@ -10,7 +10,7 @@ public class Solution {
         
         int n = A.length() -1;
         int right =  n;
-        boolean flag = true;//control the flow when it not palindrome
+        boolean flag = true;//control the flow when is not palindrome.
         for(int left = 0 ; left <= n; left++){
             if(A.charAt(left) == A.charAt(right) &&  n/2 == left){    
               // n/2 = divide the word in half and verify the length is equal to iterator variables either left or right we can use.
