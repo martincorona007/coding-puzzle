1472fd9730f1626b9e32de2f68aaf90eefde8c90
martincorona007 <martincorona007@hotmail.com>
2025-10-01 10:54:47 -0600

    CW - adding Junit 4

---
+++ Codewars/lib/src/test/java/org/example/ReturnNegativeTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ReturnNegativeTest {
+    private ReturnNegative returnNeative;
+   
+    @Test
+    public void test1() {
+        assertEquals(-42, returnNeative.makeNegative(42));
+    }
+    @Test
+    public void test2() {
+        assertEquals(-1, returnNeative.makeNegative(1));
+    }
+    @Test
+    public void test3() {
+        assertEquals(0, returnNeative.makeNegative(0));
+    }
+    @Test
+    public void test4() {
+        assertEquals(-5, returnNeative.makeNegative(-5));
+    }
+}
