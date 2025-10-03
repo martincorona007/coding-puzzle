aa2d4c9d6ed44b76b1af50058091aba3456f93ad
martincorona007 <martincorona007@hotmail.com>
2025-10-03 12:12:32 -0600

    LC - Contains Duplicate TC O(n) SC O(n)

---
+++ LeetCode/containsDuplicate.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class containsDuplicate {
+     public boolean containsDuplicate(int[] nums) {
+       Set<Integer> setInstance = new HashSet<>();
+
+       for(int i = 0 ; i < nums.length;i++){
+            if(setInstance.contains(nums[i])){
+                return true;
+            }else{
+                setInstance.add(nums[i]);
+            }
+       }
+       return false;
+    }
+}
