8689e6c54011ef3d4e0a2a09b564df921aec210c
martincorona007 <martincorona007@hotmail.com>
2025-10-20 17:27:18 -0600

    LC 242 Valid Anagram TC O(n) SC O(n)

---
+++ LeetCode/ValidAnagram.java
@@ -0,0 +1,32 @@
+import java.util.Collection;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> box = new HashMap<>();
+
+        for(int i = 0; i <= s.length() - 1; i++){
+            if(box.containsKey(s.charAt(i))){
+                box.put(s.charAt(i),box.get(s.charAt(i))+1);
+            }else{
+                box.put(s.charAt(i),1);
+            }
+        }
+        for(int x = 0; x <= t.length() - 1; x++){
+            if(box.containsKey(t.charAt(x))){
+                box.put(t.charAt(x),box.get(t.charAt(x))-1);
+            }else{
+                return false;
+            }
+        }
+        Collection<Integer> countWord = box.values();
+        Iterator<Integer> iterator = countWord.iterator();
+        while(iterator.hasNext()){
+            Integer score = iterator.next();
+            if(score == 0){
+
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
