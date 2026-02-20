7c338770041ee14ee8964b999658df0393aa5aeb
martincorona007 <donnadiee666@gmail.com>
2026-02-20 12:56:52 -0600

    feat(leetcode):  adding unit testing for some leetcode problems
     - Integer to English Words
     - Valid Anagram
     - Repeated DNA Sequences
     - Comment 2nd Single Number solution

--- LeetCode/JavaLeetCode/src/main/java/org/example/SingleNumber.java
+++ LeetCode/JavaLeetCode/src/main/java/org/example/SingleNumber.java
@@ -25,21 +25,21 @@ class SingleNumber {
 //Runtime: 41 ms, faster than 8.87% of Java online submissions for Single Number.
 //Memory Usage: 53.7 MB, less than 23.31% of Java online submissions for Single Number.
 
-    HashSet<Integer> singleNumber = new HashSet<Integer>();
-    public int singleNumberV2(int[] nums) {
-
-        for(int i = 0; i < nums.length;i++){
-            if(singleNumber.size() == 0){
-                singleNumber.add(nums[i]);
-            }else if(singleNumber.contains(nums[i])){
-                singleNumber.remove(nums[i]);
-            }else{
-                singleNumber.add(nums[i]);
-            }
-        }
-        for(Integer iter: singleNumber){
-            return iter;
-        }
-        return 0;
-    }
+//HashSet<Integer> singleNumber = new HashSet<Integer>();
+//    public int singleNumberV2(int[] nums) {
+//
+//        for(int i = 0; i < nums.length;i++){
+//            if(singleNumber.size() == 0){
+//                singleNumber.add(nums[i]);
+//            }else if(singleNumber.contains(nums[i])){
+//                singleNumber.remove(nums[i]);
+//            }else{
+//                singleNumber.add(nums[i]);
+//            }
+//        }
+//        for(Integer iter: singleNumber){
+//            return iter;
+//        }
+//        return 0;
+//    }
 }
